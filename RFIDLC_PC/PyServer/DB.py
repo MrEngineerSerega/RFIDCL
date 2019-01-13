fcc47744a9ff7bb53a9f75930e2cd651f0c9fcb5
Sergey <serg.miass340@gmail.com>
2019-01-13 22:39:21 +0500

    Добвлен тестовый файл для работы с файлом БД

--- RFIDLC_PC/PyServer/DB.py
+++ RFIDLC_PC/PyServer/DB.py
@@ -1,2 +1,21 @@
 import sqlite3
-https://github.com/MrEngineerSerega/RFIDCL
+
+limit = input("лимит выборки")
+
+conn = sqlite3.connect("Chinook_Sqlite.sqlite")
+cursor = conn.cursor()
+
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 5")
+results = cursor.fetchall()
+
+print(results)
+
+cursor.execute("INSERT INTO Artist VALUES (Null, 'fdgdfgdfg')")
+conn.commit()
+
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT :limit", {"limit": int(limit)})
+results = cursor.fetchall()
+
+print(results)
+
+conn.close()
