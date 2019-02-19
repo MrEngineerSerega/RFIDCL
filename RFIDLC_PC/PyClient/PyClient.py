9bff172c261493f79e2141f8b149c209a9b83e60
Sergey <serg.miass340@gmail.com>
2019-02-19 22:09:29 +0500

    Реализованно шифрование сообщений между сервером и клиентом

--- RFIDLC_PC/PyClient/PyClient.py
+++ RFIDLC_PC/PyClient/PyClient.py
@@ -3,11 +3,12 @@ import serial.tools.list_ports
 import time
 from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
+from Crypto.Cipher import PKCS1_OAEP
 import logging
 from tornado.ioloop import IOLoop
 from tornado import gen
 from tornado.tcpclient import TCPClient
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -36,16 +37,42 @@ def DetectingArduino():
 @gen.coroutine
 def send_message():
     stream = yield TCPClient().connect("localhost", "8888")
+
+    yield stream.write(RSAPublicKey + b'\r\n')
+    encSessionKey = yield stream.read_until(b"\r\n")
+    encSessionKey = encSessionKey.replace(b'\r\n', b'')
+    RSACipher = PKCS1_OAEP.new(RSAPrivateKey)
+    sesionKey = RSACipher.decrypt(encSessionKey)
+
+    EncAESCipher = AES.new(sesionKey, AES.MODE_EAX)
+    nonce = EncAESCipher.nonce
+    DecAESCipher = AES.new(sesionKey, AES.MODE_EAX, nonce = nonce)
     while True:
         arduinoRecive = ser.readline()
         if arduinoRecive.decode() != "":
-            message = arduinoRecive
-            yield stream.write(message)
-            logging.info("Sent to server: %s", message)
-            reply = yield stream.read_until(b"\r\n")
-            logging.info("Response from server: %s", reply.decode().strip())
+            encMessage, tag = EncAESCipher.encrypt_and_digest(arduinoRecive)
+            yield stream.write(encMessage + b'\r\n')
+            logging.info("Sent to server: %s", encMessage + b'\r\n')
+            encResponse = yield stream.read_until(b"\r\n")
+            encResponse = encResponse.replace(b'\r\n', b'')
+            logging.info("Response from server: %s", encResponse)
+            response = DecAESCipher.decrypt(encResponse)
+            logging.info(response)
+
+
+
+
+
+
+
+
+
+logging.info("Programm is started")
+RSAPrivateKey = RSA.generate(2048)
+logging.debug("Private key: %s", RSAPrivateKey)
+RSAPublicKey = RSAPrivateKey.publickey().export_key()
+logging.debug("Public key: %s", RSAPublicKey)
 
 
 ser = serial.Serial(DetectingArduino(), 115200, timeout=1)
-
 IOLoop.current().run_sync(send_message)
--- RFIDLC_PC/PyServer/PyServer.py
+++ RFIDLC_PC/PyServer/PyServer.py
@@ -4,6 +4,10 @@ from tornado import gen
 from tornado.iostream import StreamClosedError
 from tornado.tcpserver import TCPServer
 from tornado.options import options, define
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 define("port", default=8888, help="TCP port to listen on")
 logging.basicConfig(
@@ -17,19 +21,25 @@ class EchoServer(TCPServer):
     def handle_stream(self, stream, address):
         clientIP = address[0]
         logging.info("%s has been connected", clientIP)
-        while True:
-            try:
+        try:
+            RSAExpPublicKey = yield stream.read_until(b"\r\n")
+            RSAPublicKey = RSA.import_key(RSAExpPublicKey)
+            logging.info("Received rsa public key from %s: %s" % (clientIP, RSAExpPublicKey))
+            sesionKey = get_random_bytes(32)
+            RSACipher = PKCS1_OAEP.new(RSAPublicKey)
+            encSessionKey = RSACipher.encrypt(sesionKey)
+            yield stream.write(encSessionKey + b'\r\n')
+            logging.info("Sent to %s: %s" % (clientIP, encSessionKey))
+            while True:
                 data = yield stream.read_until(b"\r\n")
                 logging.info("Received bytes from %s: %s" % (clientIP, data))
                 if not data.endswith(b"\r\n"):
                     data = data + b"\r\n"
                 yield stream.write(data)
-            except StreamClosedError:
-                logging.warning("Lost client at host %s", address[0])
-                break
-            except Exception as e:
-                logging.error(e)
-
+        except StreamClosedError:
+            logging.warning("Lost client at host %s", clientIP)
+        # except Exception as e:
+        #     logging.error(e)
 
 if __name__ == "__main__":
     options.parse_command_line()
