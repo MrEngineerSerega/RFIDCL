f1cf44f59943663b4378cb85f48b74d32e84f713
Сергей <serg.miass340@gmail.com>
2020-11-04 15:50:36 +0500

    Encrypt

--- RFIDLC_PC/PyClient/client.py
+++ RFIDLC_PC/PyClient/client.py
@@ -1,17 +1,63 @@
 import asyncio
+import rsa
+from Crypto.Cipher import AES
+from Crypto import Random
 
 SERVER_IP = "localhost"
 SERVER_PORT = 8888
+
+
+class Encryptor:
+    def __init__(self):
+        self.public_key = None
+        self.private_key = None
+        self.aes = None
+
+    def set_rsa_pub_key(self, key):
+        self.public_key = rsa.PublicKey.load_pkcs1(key)
+
+    def create_rsa_pair(self, size=512):
+        self.public_key, self.private_key = rsa.newkeys(size)
+        return self.public_key.save_pkcs1()
+
+    def rsa_encrypt(self, data: bytes):
+        return rsa.encrypt(data, self.public_key)
+
+    def rsa_decrypt(self, data: bytes):
+        return rsa.decrypt(data, self.private_key)
+
+    def create_aes(self, size=24):
+        key = Random.new().read(size)
+        self.aes = AES.new(key, AES.MODE_EAX)
+        return key, self.aes.nonce
+
+    def set_aes(self, key, nonce):
+        self.aes = AES.new(key, AES.MODE_EAX, nonce=nonce)
+
+    def aes_encrypt(self, data):
+        return b"".join(self.aes.encrypt_and_digest(data))
+
+    def aes_decrypt(self, data):
+        # self.aes.verify(tag)
+        return self.aes.decrypt(data[:-16])
 
 
 async def main(loop):
     reader, writer = await asyncio.open_connection(SERVER_IP, SERVER_PORT,
                                                    loop = loop)
 
-    writer.write(b"hello world\n")
+    encryption = Encryptor()
+    key, nonce = encryption.create_aes()
 
-    data = await reader.read(1024)
-    print(data)
+    encryption.set_rsa_pub_key(await reader.read(1024))
+
+    writer.write(encryption.rsa_encrypt(key + nonce))
+    await writer.drain()
+
+    await reader.read(1024)
+
+    writer.write(encryption.aes_encrypt(b"helloworld"))
+    await writer.drain()
 
     writer.close()
 
--- RFIDLC_PC/PyServer/server.py
+++ RFIDLC_PC/PyServer/server.py
@@ -1,17 +1,62 @@
 import asyncio
+import rsa
+from Crypto.Cipher import AES
+from Crypto import Random
 
 PORT = 8888
 
+
+class Encryptor:
+    def __init__(self):
+        self.public_key = None
+        self.private_key = None
+        self.aes = None
+
+    def set_rsa_pub_key(self, key):
+        self.public_key = rsa.PublicKey.load_pkcs1(key)
+
+    def create_rsa_pair(self, size=512):
+        self.public_key, self.private_key = rsa.newkeys(size)
+        return self.public_key.save_pkcs1()
+
+    def rsa_encrypt(self, data: bytes):
+        return rsa.encrypt(data, self.public_key)
+
+    def rsa_decrypt(self, data: bytes):
+        return rsa.decrypt(data, self.private_key)
+
+    def create_aes(self, size=24):
+        key = Random.new().read(size)
+        self.aes = AES.new(key, AES.MODE_EAX)
+        return key, self.aes.nonce
+
+    def set_aes(self, key, nonce):
+        self.aes = AES.new(key, AES.MODE_EAX, nonce=nonce)
+
+    def aes_encrypt(self, data):
+        return "".join(self.aes.encrypt_and_digest(data))
+
+    def aes_decrypt(self, data):
+        # self.aes.verify(tag)
+        return self.aes.decrypt(data[:-16])
 
 async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     addr = writer.get_extra_info("peername")
     print("Incoming connection from: {}:{}".format(addr[0], addr[1]))
 
-    data = await reader.read(1024)
-    print("Received bytes: {}".format(data))
+    encryption = Encryptor()
+    pub = encryption.create_rsa_pair()
 
-    writer.write(data.upper())
+    writer.write(pub)
     await writer.drain()
+
+    aes_data = encryption.rsa_decrypt(await reader.read(1024))
+    encryption.set_aes(aes_data[:-16], aes_data[-16:])
+
+    writer.write(b"ok")
+    await writer.drain()
+
+    print(encryption.aes_decrypt(await reader.read(1024)))
 
     writer.close()
 
